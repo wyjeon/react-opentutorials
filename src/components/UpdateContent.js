6c3b9b33cbe1647aebb3b5bac0bab894dc413027
wyjeon <wyjeon.info@gmail.com>
2021-02-04 18:50:36 +0900

    React - 20.2. update 구현 : form

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class UpdateContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.data.title,
+      desc: this.props.data.desc
+    };
+    this.inputFormHandler = this.inputFormHandler.bind(this);
+  }
+  inputFormHandler(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
   render() {
     console.log(this.props.data);
     return (
@@ -15,10 +26,23 @@ class UpdateContent extends Component {
           }.bind(this)}
         >
           <p>
-            <input type="text" name="title" placeholder="title"></input>
+            <input
+              type="text"
+              name="title"
+              placeholder="title"
+              value={this.state.title}
+              onChange={this.inputFormHandler}
+            ></input>
           </p>
           <p>
-            <textarea name="desc" placeholder="description"></textarea>
+            <textarea
+              name="desc"
+              placeholder="description"
+              value={this.state.desc}
+              onChange={this.inputFormHandler}
+            >
+              {this.state.desc}
+            </textarea>
           </p>
           <p>
             <input type="submit"></input>
