a5cc1b0d566008f61fb03ba4af2977ac753fcbc9
wyjeon <wyjeon.info@gmail.com>
2021-02-04 14:50:35 +0900

    React - 15.2. State 사용

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //state값 을 초기화
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web" }
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web" />
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        />
         <TOC />
         <Content title="HTML" desc="HTML is..." />
       </div>
