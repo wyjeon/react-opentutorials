510fefa9417bbaea7e5d1d52e9b77bfc7311de5f
wyjeon <wyjeon.info@gmail.com>
2021-02-04 14:12:14 +0900

    React - 11.2. 컴포넌트 만들기 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
+//Class Name은 대문자
+class Subject extends Component {
+  //class안의 fuction render()은 function키워드 생략가능
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        World Wide Web!
+      </header>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <div className="App">React!!</div>;
+    return (
+      <div className="App">
+        <Subject />
+        <Content />
+      </div>
+    );
   }
 }
 
